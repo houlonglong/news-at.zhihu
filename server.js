1df4a215279152b67f56154730d266673bedbe45
houlonglong <31363537@qq.com>
2016-11-06 00:10:05 +0800

    app.js 版本恢复

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-var express = require('express')
-var app = new express()
-//反向代理
-var request = require('request')
-var app = new express()
-app.use(express.static('public'))
+var express = require('express');
+var request = require('request');
+var app = new express();
 
-/*默认静态路进*/
-app.get('/',function(req,res,next){
-	res.json(req.headers)
-})
-var apiBaseUrl = 'http://news-at.zhihu.com'
-app.get(/^\/api\/.+$/,function(req,res,next){
-	console.log('porxy:{%$ => %$}','-----------',req.url,apiBaseUrl+req.url)        
-	request.get(apiBaseUrl+req.url,function(err,response,body){
-		if(err){
-			console.log(err)
-			res.json({})
-		}else{
-			res.send(body)
-		}
-	})
-})
+app.use(express.static('public'));
 
-//反倒链
+app.get('/', function (req, res, next) {
+    res.json(req.headers);
+});
+
+var apiBaseUrl = 'http://news-at.zhihu.com';
+app.get(/^\/api\/.+$/, function (req, res, next) {
+	console.log('proxy: { %s => %s }', req.url, apiBaseUrl + req.url);
+	request.get(apiBaseUrl + req.url).pipe(res);
+});
+
 app.get('/img/proxy', function (req, res, next) {
 	if (!req.query.img) {
 		res.status(400);
@@ -32,8 +23,9 @@ app.get('/img/proxy', function (req, res, next) {
 	request.get({ url: req.query.img, headers: { referrer: 'http://news-at.zhihu.com/' }}).pipe(res);
 });
 
-app.listen(3000,function(){
-	console.log('app.js+zepto app ready port 3000')
-})
+app.listen(3000, function () {
+    console.log('app.js + zepto app ready at port 3000');
+});
 
-module.exports = app ;
+module.exports = app;
+
